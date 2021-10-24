75b5c1df99f6b3c4fd7e1d279f40f95c7cd67fdf
Nandinee Sahoo <sahoonandinee@gmail.com>
2021-10-24 16:37:53 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String args[]){
+        int a = 4;
+        int b  = 5;
+        int sum = a + b;
+        system.out.println ("Sum is "+ sum);
+    }
+}
